4cf8b471826d3a3ef277f45e49241817b165a326
bunseokbot <admin@smishing.kr>
2017-06-08 23:19:00 +0900

    Add flask server and config

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, json, abort
+import requests
+import yaml
+
+app = Flask(__name__)
+
+APIS = {
+    "coinone": "https://api.coinone.co.kr/ticker?currency=all"
+}
+
+config = yaml.load(open('config.yml').read())
+
+
+def get_coininfo():
+    msg = ""
+
+    try:
+        data = requests.get(APIS['coinone']).json()
+        msg += "BTC : {} Won (Volume : {} BTC)\n".format(data['btc']['last'], data['btc']['volume'])
+        msg += "ETH : {} Won (Volume : {} ETH)\n".format(data['eth']['last'], data['eth']['volume'])
+        msg += "ETC : {} Won (Volume : {} ETC)\n".format(data['etc']['last'], data['etc']['volume'])
+        msg += "XRP : {} Won (Volume : {} XRP)".format(data['xrp']['last'], data['xrp']['volume'])
+    except:
+        msg += "Error while loading currency info"
+
+    return msg
+
+
+@app.route("/")
+def hello():
+    return 'hello?'
+
+
+@app.route("/coin")
+def coin():
+    if request.form.get('token') == config['token']:
+        coinmsg = get_coininfo()
+
+        response = {
+            'response_type': 'in_channel',
+            'text': 'Current coinone market status',
+            'attachments': [
+                {
+                    'text': coinmsg
+                }
+            ]
+        }
+
+        return app.response_class(
+            response=json.dumps(response),
+            status=200,
+            mimetype="application/json"
+        )
+    else:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80, threaded=True)
